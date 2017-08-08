908f8562d5a5bab175703f87aa4ac93242666dc6
Jo Dickson <jogreybill@gmail.com>
2017-08-08 12:06:48 -0500

    Converted layout actions to filters

--- layouts/ucf-post-search.php
+++ layouts/ucf-post-search.php
@@ -2,21 +2,21 @@
 
 if ( !function_exists( 'ucf_post_list_search_before' ) ) {
 
-	function ucf_post_list_search_before( $posts, $atts ) {
+	function ucf_post_list_search_before( $content, $posts, $atts ) {
 		ob_start();
 	?>
 	<div class="ucf-post-search">
 	<?php
-		echo ob_get_clean();
+		return ob_get_clean();
 	}
 
-	add_action( 'ucf_post_list_search_before', 'ucf_post_list_search_before', 10, 2 );
+	add_filter( 'ucf_post_list_search_before', 'ucf_post_list_search_before', 10, 3 );
 
 }
 
 if ( !function_exists( 'ucf_post_list_search' ) ) {
 
-	function ucf_post_list_search( $posts, $atts ) {
+	function ucf_post_list_search( $content, $posts, $atts ) {
 		if ( ! is_array( $posts ) && $posts !== false ) { $posts = array( $posts ); }
 		ob_start();
 	?>
@@ -26,16 +26,16 @@ if ( !function_exists( 'ucf_post_list_search' ) ) {
 			</div>
 		<?php endif; ?>
 	<?php
-		echo ob_get_clean();
+		return ob_get_clean();
 	}
 
-	add_action( 'ucf_post_list_search', 'ucf_post_list_search', 10, 2 );
+	add_filter( 'ucf_post_list_search', 'ucf_post_list_search', 10, 3 );
 
 }
 
 if ( !function_exists( 'ucf_post_list_search_script' ) ) {
 
-	function ucf_post_list_search_script( $posts, $atts, $typeahead_settings ) {
+	function ucf_post_list_search_script( $content, $posts, $atts, $typeahead_settings ) {
 		if ( ! is_array( $posts ) && $posts !== false ) { $posts = array( $posts ); }
 		ob_start();
 		if ( $posts ):
@@ -54,23 +54,23 @@ if ( !function_exists( 'ucf_post_list_search_script' ) ) {
 	<?php
 		endif;
 
-		echo ob_get_clean();
+		return ob_get_clean();
 	}
 
-	add_action( 'ucf_post_list_search_script', 'ucf_post_list_search_script', 10, 3 );
+	add_filter( 'ucf_post_list_search_script', 'ucf_post_list_search_script', 10, 4 );
 
 }
 
 if ( !function_exists( 'ucf_post_list_search_after' ) ) {
 
-	function ucf_post_list_search_after( $posts, $atts ) {
+	function ucf_post_list_search_after( $content, $posts, $atts ) {
 		ob_start();
 	?>
 	</div>
 	<?php
-		echo ob_get_clean();
+		return ob_get_clean();
 	}
 
-	add_action( 'ucf_post_list_search_after', 'ucf_post_list_search_after', 10, 2 );
+	add_filter( 'ucf_post_list_search_after', 'ucf_post_list_search_after', 10, 3 );
 
 }
