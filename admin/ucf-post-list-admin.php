86656774e5b32a60118ef8d6a121011a5d2dfb11
Jo Dickson <jogreybill@gmail.com>
2021-01-15 11:30:14 -0500

    Added cachebusting to plugin-specific enqueued assets

--- admin/ucf-post-list-admin.php
+++ admin/ucf-post-list-admin.php
@@ -6,6 +6,9 @@ if ( ! class_exists( 'UCF_Post_List_Admin' ) ) {
 	class UCF_Post_List_Admin {
 		public static function enqueue_admin_scripts() {
 			if ( is_admin() ) {
+				$plugin_data = get_plugin_data( UCF_POST_LIST__PLUGIN_FILE, false, false );
+				$version     = $plugin_data['Version'];
+
 				if ( function_exists( 'wp_enqueue_media' ) ) {
 					wp_enqueue_media();
 				} else {
@@ -14,7 +17,7 @@ if ( ! class_exists( 'UCF_Post_List_Admin' ) ) {
 					wp_enqueue_script( 'thickbox' );
 					wp_enqueue_media();
 				}
-				wp_enqueue_script( 'ucf-post-list-admin', plugins_url( 'static/js/ucf-post-list-admin.min.js', UCF_POST_LIST__PLUGIN_FILE ), array(), null, true );
+				wp_enqueue_script( 'ucf-post-list-admin', plugins_url( 'static/js/ucf-post-list-admin.min.js', UCF_POST_LIST__PLUGIN_FILE ), array(), $version, true );
 			}
 		}
 	}
